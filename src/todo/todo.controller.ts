599c4e11deab19a181e0b4b6d456288d35e3c393
Mohammad Ahmad <m.ahmad8929@gmail.com>
2024-02-26 22:23:32 +0530

    Added guards and updated schema

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -9,28 +9,28 @@ import {
   UseGuards,
   Req,
   Res,
-} from '@nestjs/common';
-import { AuthGuard } from 'src/guards/user';
-import { TodoService } from './todo.service';
-import { CreateTodoDto } from './dto/create-todo.dto';
-import { UpdateTodoDto } from './dto/update-todo.dto';
-import { UserService } from 'src/user/user.service';
+} from "@nestjs/common";
+import { AuthGuard } from "src/guards/user";
+import { TodoService } from "./todo.service";
+import { CreateTodoDto } from "./dto/create-todo.dto";
+import { UpdateTodoDto } from "./dto/update-todo.dto";
+import { UserService } from "src/user/user.service";
 
-@Controller('todo')
+@Controller("todo")
 export class TodoController {
   constructor(
     private readonly todoService: TodoService,
-    private readonly userService: UserService,
+    private readonly userService: UserService
   ) {}
 
   @Post()
   @UseGuards(AuthGuard)
   async create(@Req() Req, @Res() res, @Body() createTodoDto: CreateTodoDto) {
     try {
-      const author = await this.userService.findOne('email', Req.user.email);
-      createTodoDto['author'] = author;
+      const author = await this.userService.findOne("email", Req.user.email);
+      createTodoDto["author"] = author;
       await this.todoService.create(createTodoDto);
-      return res.json({ message: 'Todo added successfully!' });
+      return res.json({ message: "Todo added successfully!" });
     } catch (err: any) {
       return res.json(err);
     }
@@ -40,21 +40,21 @@ export class TodoController {
   @UseGuards(AuthGuard)
   async findAll(@Req() req, @Res() res) {
     try {
-      const author = await this.userService.findOne('email', req.user.email);
+      const author = await this.userService.findOne("email", req.user.email);
       const todos = await this.todoService.findAll(author);
-      return res.json({ message: 'Todos fetched successfully!', todos });
+      return res.json({ message: "Todos fetched successfully!", todos });
     } catch (err: any) {
       return res.json(err);
     }
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateTodoDto: UpdateTodoDto) {
     return this.todoService.update(id, updateTodoDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.todoService.remove(id);
   }
 }
